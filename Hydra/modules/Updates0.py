9a1b87535cb1b9d81af527fe52e55dea8e58f7cd
OTAZUKIII <115762083+Otazuki004@users.noreply.github.com>
2023-02-16 17:36:28 +0530

    Update Updates0.py

--- Hydra/modules/Updates0.py
+++ Hydra/modules/Updates0.py
@@ -19,18 +19,3 @@ async def awake(event):
     * You can Rename Files ✅ *
 Powered by: @Toon_LinkZ ™ """
     await tbot.send_file(event.chat_id, PHOTO1, caption=HYDRA1)
-    
-__mod_name__ = "Rename"
-    
-__help__ = """
-*Files Rename:* 
-• How to Rename 
-Send Some File It's Ask Rename
-Click Rename And Send Your New Title For Your file And And Select Document Or Video After Selecting Your File Will Upload Few minutes
-
- • How to Set Thumbnail
- Send A Photo It's Save Your Thumbnail
-
-°᭄Powered by @Toon_LinkZ™
-"""
-
