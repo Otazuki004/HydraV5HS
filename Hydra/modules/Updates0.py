83ebeed4f748579ac33f8a2c58ffa3c6b4c3cea4
OTAZUKIII <115762083+Otazuki004@users.noreply.github.com>
2023-03-01 20:47:45 +0530

    5.5

---
+++ Hydra/modules/Updates0.py
@@ -0,0 +1,21 @@
+from Hydra import tbot as tbot
+from Hydra.events import register
+
+PHOTO1 = "https://te.legra.ph/file/c4b3a0fb319744a2e41fd.jpg"
+
+
+@register(pattern=("/updates"))
+async def awake(event):
+    HYDRA1 = f"""
+    
+    * Hexa Info Coming Soon ✅ *
+    * Truth or Dare Feature Added ✅ *
+    * Welcome (Spam) Removed ✅ *
+    * Welcome Messages Are Changed✅ *
+    * Music Feature Coming Soon✅ *
+    * Running On New Repo✅ *
+    * Almost All Errors Fixed ✅ *
+    * UI Changed ✅ *
+    * You can Rename Files ✅ *
+Powered by: @Toon_LinkZ ™ """
+    await tbot.send_file(event.chat_id, PHOTO1, caption=HYDRA1)
