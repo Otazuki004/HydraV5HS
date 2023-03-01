ada862004b16396b743e0c92ad0e6734fe602262
OTAZUKIII <115762083+Otazuki004@users.noreply.github.com>
2023-03-01 17:49:25 +0530

    Add files via upload

---
+++ Hydra/modules/Toools.py
@@ -0,0 +1,32 @@
+import random
+from Hydra.events import register
+
+
+ran1 = "1234567890"
+ran2 = "¥€¢£"
+ran3 = "@#$&+?!"
+ran4 = "()[]{}"
+ran5 = "NOPQRSTUVWXYZ"
+ran6 = "Otazuki"
+ran7 = "ABCDEFGHIJKLM"
+ran8 = "uvwxyz"
+ran9 = "ghijklmnopqrst"
+ran0 = "abcdef"
+ran11 = "mairupunda"
+
+all = ran1 + ran2 + ran3 + ran4 + ran5 + ran6 + ran7 + ran8 + ran9 + ran0 + ran11
+
+length = 11
+
+@register(pattern=("/pass"))
+async def awake(event):
+   password = "".join(random.sample(all, length))
+   await event.reply(password)
+   
+   __mod_name__ = "Toools"
+
+__help__ = """
+ ❍ /pass - Bot Send You Random Password
+ 
+ @Toon_LinkZ™
+"""
